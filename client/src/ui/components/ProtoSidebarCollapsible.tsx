ab1c43f9ae5096a4c643b8ae13a1047b57a524c1
Vihar Desu <vihardesu@gmail.com>
2025-04-15 15:39:15 -0400

    fix

--- client/src/ui/components/ProtoSidebarCollapsible.tsx
+++ client/src/ui/components/ProtoSidebarCollapsible.tsx
@@ -7,6 +7,9 @@
 
 import React from "react";
 import * as SubframeUtils from "../utils";
+import { IconButton } from "./IconButton";
+import { FeatherMenu } from "@subframe/core";
+import { FeatherX } from "@subframe/core";
 import * as SubframeCore from "@subframe/core";
 import { FeatherCircleDashed } from "@subframe/core";
 
@@ -66,10 +69,7 @@ const NavItem = React.forwardRef<HTMLElement, NavItemProps>(function NavItem(
 
 interface ProtoSidebarCollapsibleRootProps
   extends React.HTMLAttributes<HTMLElement> {
-  header?: React.ReactNode;
-  children?: React.ReactNode;
   expanded?: boolean;
-  footer?: React.ReactNode;
   className?: string;
 }
 
@@ -78,10 +78,7 @@ const ProtoSidebarCollapsibleRoot = React.forwardRef<
   ProtoSidebarCollapsibleRootProps
 >(function ProtoSidebarCollapsibleRoot(
   {
-    header,
-    children,
     expanded = false,
-    footer,
     className,
     ...otherProps
   }: ProtoSidebarCollapsibleRootProps,
@@ -103,21 +100,69 @@ const ProtoSidebarCollapsibleRoot = React.forwardRef<
           { "w-60 grow shrink-0 basis-0": expanded }
         )}
       >
-        {header ? (
-          <div className="flex w-full flex-col items-center justify-center gap-2 px-5 py-6">
-            {header}
+        <div className="flex w-full flex-col items-center justify-center gap-2 px-5 py-6">
+          <div
+            className={SubframeUtils.twClassNames(
+              "flex items-center justify-center gap-2",
+              { "h-auto w-full flex-none items-center justify-end": expanded }
+            )}
+          >
+            <IconButton
+              className={SubframeUtils.twClassNames("h-auto w-auto flex-none", {
+                "hidden h-auto w-auto flex-none": expanded,
+              })}
+              disabled={false}
+              variant="neutral-tertiary"
+              size="large"
+              icon={<FeatherMenu />}
+            />
+            <IconButton
+              className={SubframeUtils.twClassNames(
+                "hidden h-auto w-auto flex-none",
+                { flex: expanded }
+              )}
+              disabled={false}
+              variant="neutral-tertiary"
+              size="large"
+              icon={<FeatherX />}
+            />
           </div>
-        ) : null}
-        {children ? (
-          <div className="flex w-full grow shrink-0 basis-0 flex-col items-start gap-1 px-3 py-4 overflow-auto">
-            {children}
+        </div>
+        <div className="flex w-full grow shrink-0 basis-0 flex-col items-start gap-1 px-3 py-4 overflow-auto">
+          <NavItem>Item</NavItem>
+        </div>
+        <div className="flex w-full items-center justify-center gap-4 overflow-hidden border-t border-solid border-neutral-border">
+          <div
+            className={SubframeUtils.twClassNames(
+              "flex h-12 grow shrink-0 basis-0 items-center gap-6 px-2 py-2",
+              {
+                "h-12 grow shrink-0 basis-0 flex-row flex-nowrap gap-6 px-0 py-0":
+                  expanded,
+              }
+            )}
+          >
+            <div
+              className={SubframeUtils.twClassNames("flex items-center gap-6", {
+                "h-auto grow shrink-0 basis-0 items-center justify-start self-stretch px-2 py-2":
+                  expanded,
+              })}
+            >
+              <img
+                className={SubframeUtils.twClassNames("flex-none", {
+                  "hidden h-12 w-8 flex-none object-cover": expanded,
+                })}
+                src="https://res.cloudinary.com/subframe/image/upload/v1744735723/uploads/7701/oog2ssqmh5bmqiy3x6i1.png"
+              />
+              <img
+                className={SubframeUtils.twClassNames("flex-none", {
+                  "h-auto grow shrink-0 basis-0 self-stretch object-contain":
+                    expanded,
+                })}
+                src="https://res.cloudinary.com/subframe/image/upload/v1744735711/uploads/7701/lwsmwbsij3saygbzmlwh.png"
+              />
+            </div>
           </div>
-        ) : null}
-        {footer ? (
-          <div className="flex w-full items-center justify-center gap-4 overflow-hidden border-t border-solid border-neutral-border">
-            {footer}
-          </div>
-        ) : null}
+        </div>
       </div>
     </nav>
   );
